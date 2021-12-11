0b7806a0493ddbe9d40d67ec489ac816d3f5497d
cmrotruck <crotruck@gmail.com>
2021-12-11 16:11:58 -0500

    added nutrition facts

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,33 +1,70 @@
+var bodyEl = document.querySelector("body");
 //nutritional api connection
-var loadNutritionalData = function(data){
+var loadNutritionalData = function (data, ingredient) {
     console.log(data);
-    
+    console.log
+
+    $('.ui.modal')
+        .modal()
+    ;
+
+    // var modalEl = document.createElement("div");
+    // modalEl.classList = "ui modal";
+
+    // var closeIconEl = document.createElement("i");
+    // closeIconEl.classList = "close icon";
+    // modalEl.appendChild(closeIconEl);
+
+    // var headerEl = document.createElement("div");
+    // headerEl.className = "modalheader";
+    // headerEl.textContent = "Nutrition Facts";
+    // modalEl.appendChild(headerEl);
+
+    // var servingSizeEl = document.createElement("div");
+    // servingSizeEl.className = "serving-size";
+    // servingSizeEl.textContent = "Amount Per Serving";
+    // modalEl.appendChild(servingSizeEl);
+
+    // var calorieEl = document.createElement("div");
+    // calorieEl.className = "calorie-header";
+    // calorieEl.textContent = "Calories";
+    // modalEl.appendChild(calorieEl);
+
+
+    // bodyEl.appendChild(modalEl);
+
+
+    $('.ui.modal')
+        .modal('show')
+    ;
 }
 
-var getNutritionalFacts = function(foodId){
-    var nutritionApiUrl = "https://api.edamam.com/api/nutrition-data?app_id=1ef0bd3b&app_key=183522d5ba98d3967d9553f24e787347&nutrition-type=cooking&ingr=" + foodId;
+var getNutritionalFacts = function (ingredient) {
+    var nutritionApiUrl = "https://api.edamam.com/api/nutrition-data?app_id=1ef0bd3b&app_key=183522d5ba98d3967d9553f24e787347&nutrition-type=cooking&ingr=" + ingredient;
 
     fetch(nutritionApiUrl)
-    .then(function(response){
-        if(response.ok){
-            response.json()
-             .then(function(data) {
-                loadNutritionalData(data);
-            });
-            console.log("Connection successful!");
-        }
-        else{
-            console.log("conneciton unsuccessful");
-        }
-    })
-    .catch(function(error){
-        console.log("could not connect");
-    })
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        loadNutritionalData(data,ingredient);
+                    });
+                console.log("Connection successful!");
+            }
+            else {
+                console.log("conneciton unsuccessful");
+            }
+        })
+        .catch(function (error) {
+            console.log("could not connect");
+        })
 
 };
 
-$(".ingredients").on("click",function(event){
+$(".ingredients").on("click", function (event) {
     event.preventDefault();
-    console.log(event.target.id);
-    getNutritionalFacts(event.target.id);
-;});
+    // console.log(event.target.id);
+    // console.log(event.target.textContent);
+    getNutritionalFacts(event.target.textContent);
+    ;
+});
